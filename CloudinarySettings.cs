1ce7215c34156ef0362f3fac86a908124ed2c1ca
Selma POLAT <32098578+SelmaP@users.noreply.github.com>
2022-02-08 13:09:38 +0300

    Add files via upload

---
+++ CloudinarySettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Helpers
+{
+    public class CloudinarySettings
+    {
+        //clouda fotoğraf yükleme
+         public string CloudName { get; set; }
+         public string ApiKey { get; set; }
+         public string ApiSecret { get; set; }
+    }
+}
